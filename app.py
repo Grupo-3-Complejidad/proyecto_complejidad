b0506263d6a8116102e9162c82ff7e2fd50a6909
Edgar <u202020371@upc.edu.pe>
2023-06-15 10:24:13 -0500

    feat: filtros para el view products

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__)
 
+# Ruta principal y búsqueda
 @app.route('/')
 def index():
     # Leer los datos del archivo CSV
@@ -12,7 +13,19 @@ def index():
         for row in csv_reader:
             productos.append(row)
 
-    return render_template('index.html', productos=productos)
+    # Obtener el texto de búsqueda de la solicitud
+    query = request.args.get('query', '')
+
+    # Filtrar los productos según el texto de búsqueda
+    productos_filtrados = []
+    if query:
+        for producto in productos:
+            if query.lower() in producto[1].lower():
+                productos_filtrados.append(producto)
+    else:
+        productos_filtrados = productos
+
+    return render_template('index.html', productos=productos_filtrados)
 
 if __name__ == '__main__':
     app.run()
