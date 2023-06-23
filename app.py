17b9ce27f0195356329b4c7b8bcf3b6636657d55
Edgar <u202020371@upc.edu.pe>
2023-06-23 12:46:57 -0500

    fix: optimization of shortest_edges

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import heapq
 import graphviz as gv
 import io
 import base64
+import tempfile
 
 app = Flask(__name__)
 
@@ -16,6 +17,9 @@ class Product:
         self.category = category
         self.price = price
         self.url = url
+
+    def get_id(self):
+        return self.id
     
     def get_name(self):
         return self.name
@@ -56,13 +60,13 @@ class Graph():
     def get_shortest_edges(self, start_node_name, count=4):
         start_node = None
         for node in self.nodes.values():
-            if node.name == start_node_name:
-                start_node = node
-                break
+          if node.name == start_node_name:
+              start_node = node
+              break
 
         if start_node is None:
-            print("No se encontró ningún nodo con ese nombre.")
-            return []
+          print("No se encontró ningún nodo con ese nombre.")
+          return []
 
         distances = {node.id: float('inf') for node in self.nodes.values()}
         distances[start_node.id] = 0
@@ -71,73 +75,49 @@ class Graph():
         visited = set()
 
         while priority_queue:
-            current_distance, current_node_id = heapq.heappop(priority_queue)
+          current_distance, current_node_id = heapq.heappop(priority_queue)
 
-            if current_node_id in visited:
-                continue
+          if current_node_id in visited:
+              continue
 
-            current_node = self.get_node(current_node_id)
-            visited.add(current_node_id)
+          current_node = self.get_node(current_node_id)
+          visited.add(current_node_id)
 
-            for edge, weight in self.edges:
-                node1_id, node2_id = edge
-                if node1_id == current_node.id and node2_id not in visited:
-                    neighbor_node1 = self.get_node(node1_id)
-                    neighbor_node2 = self.get_node(node2_id)
-                    new_distance = current_distance + weight
-                    if new_distance < distances[neighbor_node2.id]:
-                        distances[neighbor_node2.id] = new_distance
-                        heapq.heappush(priority_queue, (new_distance, neighbor_node2.id))
-                elif node2_id == current_node.id and node1_id not in visited:
-                    neighbor_node1 = self.get_node(node1_id)
-                    neighbor_node2 = self.get_node(node2_id)
-                    new_distance = current_distance + weight
-                    if new_distance < distances[neighbor_node1.id]:
-                        distances[neighbor_node1.id] = new_distance
-                        heapq.heappush(priority_queue, (new_distance, neighbor_node1.id))
+          for edge, weight in self.edges:
+              node1_id, node2_id = edge
+              if node1_id == current_node.id and node2_id not in visited:
+                neighbor_node1 = self.get_node(node1_id)
+                neighbor_node2 = self.get_node(node2_id)
+                new_distance = current_distance + weight
+                if new_distance < distances[neighbor_node2.id]:
+                    distances[neighbor_node2.id] = new_distance
+                    heapq.heappush(priority_queue, (new_distance, neighbor_node2.id))
+              elif node2_id == current_node.id and node1_id not in visited:
+                  neighbor_node1 = self.get_node(node1_id)
+                  neighbor_node2 = self.get_node(node2_id)
+                  new_distance = current_distance + weight
+                  if new_distance < distances[neighbor_node1.id]:
+                      distances[neighbor_node1.id] = new_distance
+                      heapq.heappush(priority_queue, (new_distance, neighbor_node1.id))
 
             # Optimización: Remover duplicados en la cola de prioridad
-            while priority_queue and priority_queue[0][1] in visited:
-                heapq.heappop(priority_queue)
+          while priority_queue and priority_queue[0][1] in visited:
+              heapq.heappop(priority_queue)
 
-            sorted_distances = sorted(distances.items(), key=lambda x: x[1])[:count]
-            shortest_edges = []
-            for node_id, distance in sorted_distances:
-                if node_id != start_node.id:
-                    node = self.get_node(node_id)
-                    shortest_edges.append((node.id, node.get_name(), node.get_quantity(), node.get_brand(), node.get_category(), node.get_price(), node.get_url(), distance))
+          sorted_distances = sorted(distances.items(), key=lambda x: x[1])[:count]
+          shortest_edges = []
+          for node_id, distance in sorted_distances:
+            if node_id != start_node.id:
+                node = self.get_node(node_id)
+                shortest_edges.append((node, distance))
 
-        return shortest_edges 
+          return shortest_edges 
 
-def print_graph(start_node, recommended_nodes):
-    # Crear un objeto Digraph
-    graph_image = gv.Graph()
-
-    graph_image.node(start_node.id, label=start_node.id)
-    for node in recommended_nodes:
-        graph_image.node(node[0], label=node[0])
-        
-    # Agregar aristas al gráfico
-    for node in recommended_nodes:
-        node_id = node[0]
-        edge = tuple(sorted([start_node.id, node_id]))
-        weight = node[7]
-        edge_color = 'green' if weight == 3 else 'yellow' if weight == 5 else 'blue' if weight == 1 else 'red'
-        graph_image.edge(edge[0], edge[1], label=str(weight), color=edge_color)
-        
-    # Guardar el gráfico en memoria como PNG
-    image_data = graph_image.pipe(format='png')
-
-    # Codificar los datos de la imagen en base64
-    import base64
-    image_base64 = base64.b64encode(image_data).decode('utf-8')
-
-    return image_base64
 
 @app.route('/')
 def index():
     # Leer los datos del archivo CSV y construir el grafo
-    graph = build_graph_from_csv('csv_Test.csv')
+    graph = build_graph_from_csv('csv_Products.csv')
 
     # Obtener el texto de búsqueda de la solicitud
     query = request.args.get('query', '')
@@ -198,7 +178,7 @@ def add_edges(graph):
 
 def filter_products(graph, query):
     productos = []
-    with open('csv_Test.csv', 'r', encoding='utf-8') as csv_file:
+    with open('csv_Products.csv', 'r', encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='@')
         for row in csv_reader:
             productos.append(row)
@@ -213,21 +193,51 @@ def filter_products(graph, query):
 
     return productos_filtrados
 
+def print_graph(start_node, recommended_nodes):
+    # Crear un objeto Digraph
+    graph_image = gv.Graph()
+
+    graph_image.node(start_node.id, label=start_node.id)
+    for node in recommended_nodes:
+        graph_image.node(node[0], label=node[0])
+
+    # Agregar aristas al gráfico
+    for node in recommended_nodes:
+        node_id = node[0]
+        edge = tuple(sorted([start_node.id, node_id]))
+        weight = node[7]
+        edge_color = 'green' if weight == 3 else 'yellow' if weight == 5 else 'blue' if weight == 1 else 'red'
+        graph_image.edge(edge[0], edge[1], label=str(weight), color=edge_color)
+
+    # Guardar el gráfico en memoria como PNG
+    image_data = graph_image.pipe(format='png')
+
+    # Codificar los datos de la imagen en base64
+    import base64
+    image_base64 = base64.b64encode(image_data).decode('utf-8')
+
+    return image_base64
+
 @app.route('/recomendacion/<producto_id>')
 def recomendacion(producto_id):
-    graph = build_graph_from_csv('csv_Test.csv')
+    graph = build_graph_from_csv('csv_Products.csv')
     producto_seleccionado = get_producto_por_id(producto_id)
 
     if producto_seleccionado:
         shortest_edges = graph.get_shortest_edges(producto_seleccionado.name, count=4)
-        recomendaciones = [(node_id, node_name, node_quantity, node_brand, node_category, node_price, node_url, distance) for node_id, node_name, node_quantity, node_brand, node_category, node_price, node_url, distance in shortest_edges]
-        image_base64 = print_graph(producto_seleccionado, recomendaciones)  # Llamada a la función print_graph
-        return render_template('recomendation.html', producto_seleccionado=producto_seleccionado, recomendaciones=recomendaciones, graph_image=image_base64)
+        recomendaciones = []
+        print(shortest_edges)
+        for node, distance in shortest_edges:
+            recomendaciones.append((node.get_id(), node.get_name(), node.get_quantity(), node.get_brand(), node.get_category(), node.get_price(), node.get_url(), distance))
+        
+        graph_data = print_graph(producto_seleccionado, recomendaciones)
+
+        return render_template('recomendation.html', producto_seleccionado=producto_seleccionado, recomendaciones=recomendaciones, graph_data=graph_data)
     else:
         return "Producto no encontrado"
 
 def get_producto_por_id(producto_id):
-    with open('csv_Test.csv', 'r', newline='', encoding='utf-8') as csv_handle:
+    with open('csv_Products.csv', 'r', newline='', encoding='utf-8') as csv_handle:
         csv_products = csv.reader(csv_handle, delimiter='@')
         for line in csv_products:
             if len(line) >= 7:
